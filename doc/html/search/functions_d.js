df7cb3a4d3ffe407dc78fc9853f0e690867ab703
pivaldi <contact@piprime.fr>
2019-04-18 11:35:37 +0200

    Generate Doc

---
+++ doc/html/search/functions_d.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['read_5fkey',['read_key',['../group__stdin.html#ga13f97ec87f85badbce75a410d1ee9d03',1,'bsfl.sh']]],
+  ['read_5fpath',['read_path',['../group__stdin.html#ga48cdd2c9387240df93e79364425097f3',1,'bsfl.sh']]]
+];
